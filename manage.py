f659cb8b0b4848de54738ea2bcfac455ff7d100e
Jay Luker <lbjay@reallywow.com>
2015-01-30 12:37:22 -0500

    mis-configured defaults for DJANGO_SETTINGS_MODULE

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import dotenv
 dotenv.read_dotenv()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dce_course_admin.settings.base")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dce_course_admin.settings")
 
     from django.core.management import execute_from_command_line
 
